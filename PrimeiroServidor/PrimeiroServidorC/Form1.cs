e1584d5932c0d7e90498cac15a32ea827cd08706
Joao gomes <heitor.parafuso@gmail.com>
2017-02-13 23:50:38 +0000

    Base feita do nosso server

---
+++ PrimeiroServidor/PrimeiroServidorC/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace PrimeiroServidorC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            IPAddress[] localIP = Dns.GetHostAddresses(Dns.GetHostName()); // saber o meu proprio IP
+
+            foreach(IPAddress address in localIP)
+            {
+                if(address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    textBox3.Text = address.ToString();
+
+                }
+
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)    // Ligar o Servidor
+        {
+
+
+
+        }
+    }
+}
